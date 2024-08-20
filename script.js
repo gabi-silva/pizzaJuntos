4848e784928c83abf55d95dfa956066f6de59d53
gabi92708 <emegabi88@gmail.com>
2024-08-20 12:15:09 -0300

    Ingreso cambios en contacto, footer y nosotros

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// Array de imágenes
+const images = [
+  './img/nosotros/mozos01.png',
+  './img/nosotros/mozos02.jpg',
+  './img/nosotros/mozos03.jpg',
+  './img/nosotros/mozos04.jpg',
+  './img/nosotros/mozos05.jpg'
+];
+
+const imgElement = document.getElementById('dynamic-image');
+
+function changeImage() {
+  // Desvanecer la imagen actual
+  imgElement.classList.add('fade-out');
+
+  // Esperar hasta que la animación de desvanecimiento termine
+  setTimeout(() => {
+    // Cambiar la imagen a una nueva de forma aleatoria
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    imgElement.src = randomImage;
+
+    // Desvanecer la imagen nueva
+    imgElement.classList.remove('fade-out');
+    imgElement.classList.add('fade-in');
+  }, 1000); // Coincide con el tiempo de animación de fadeOut
+}
+
+// Cambiar la imagen cada 5 segundos
+setInterval(changeImage, 5000);
+
+
 document.getElementById('whatsappForm').addEventListener('submit', function(e) {
     e.preventDefault(); // Evitar el envío predeterminado del formulario
   
